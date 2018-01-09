5897525265853c3e1c32f9307d6581f30a1461ef
Jo Christian Sandvik <jochristian.sandvik@gmail.com>
2018-01-09 23:02:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function destructivelyAppendKitten(kitten) {
   kittens.push(kitten);
   return kittens;
 }
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(kitten) {
+  kittens.pop(kitten);
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(kitten) {
+  kittens.shift(kitten);
+  return kittens;
+}
